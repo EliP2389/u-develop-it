94f45a17145879884307fcb2252227a50d5c6a22
Eli Pagan <mbk8689@gmail.com>
2021-12-15 13:14:58 -0500

    used JOIN statments and a new table that uses foreinkey to work together

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
 create TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name_ VARCHAR(50) NOT NULL,
@@ -5,8 +8,11 @@ create TABLE parties (
 );
 
 CREATE TABLE candidates (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
